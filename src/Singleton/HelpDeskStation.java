1f38c82b2d18fc806db18c038b1ff7702f389ae9
John Lemuel Tapel <142371919+Lemuel21@users.noreply.github.com>
2023-12-06 00:16:35 +0800

    Update HelpDeskStation.java

--- src/Singleton/HelpDeskStation.java
+++ src/Singleton/HelpDeskStation.java
@@ -1,2 +1,17 @@
-package Singleton;public class HelpDeskStation {
+package Singleton;
+
+public class HelpDeskStation {
+    public static void main(String[] args) {
+        QueueManager queueManager = QueueManager.getInstance();
+        queueManager.getCurrentQueueNumber("Station 3");
+        queueManager.getCurrentQueueNumber("Station 4");
+        queueManager.getCurrentQueueNumber("Station 1");
+
+        queueManager.resetQueueNumber("Station 1", 100);
+
+        queueManager.getCurrentQueueNumber("Station 4");
+        queueManager.getCurrentQueueNumber("Station 1");
+        queueManager.getCurrentQueueNumber("Station 2");
+    }
 }
+
