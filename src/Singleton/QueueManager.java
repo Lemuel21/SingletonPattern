a176d39463596b8e6056c506c4ae5df109ef342b
John Lemuel Tapel <142371919+Lemuel21@users.noreply.github.com>
2023-12-06 00:20:33 +0800

    Update QueueManager.java

--- src/Singleton/QueueManager.java
+++ src/Singleton/QueueManager.java
@@ -1,2 +1,34 @@
-package Singleton;public class QueueManager {
+package Singleton;
+
+public class QueueManager {
+    private static QueueManager instance;
+    private int currentQueueNumber;
+
+    private QueueManager() {
+
+    }
+
+    public static QueueManager getInstance() {
+        if (instance == null)
+            instance = new QueueManager();
+
+         return instance;
+    }
+
+    // get the current queue number
+    public void getcurrentqNumber(String station) {
+        System.out.println("Queue number " + currentQueueNumber
+                + " is in already " + station + ".");
+        incrementQueueNumber();
+    }
+
+    public void incrementqNumber()   {
+        currentQueueNumber++;
+    }
+
+    public void resetqNumber(String station, int resetQueue) {
+        System.out.println(station + " reset the queuing number to " + resetQueue);
+        this.currentQueueNumber = resetQueue;
+    }
 }
+
